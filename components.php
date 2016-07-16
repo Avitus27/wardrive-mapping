ebf21933689b6f05fbd25fefd391f89c4ff0f9eb
Mervyn Galvin <mervyn511@gmail.com>
2016-07-17 00:27:55 +0100

    Various fixes
    - Add comments to describe the process (components.php)
    - Describe if an env variable is required or not (.env.example)
    - Only load mapSign.php if we have a secret key (components.php)

--- components.php
+++ components.php
@@ -1,7 +1,9 @@
 <?php
-	require("vendor/autoload.php");
-	$dotenv = new Dotenv\Dotenv(__DIR__);
+	require("vendor/autoload.php"); // Loads anything that's been added via composer
+	$dotenv = new Dotenv\Dotenv(__DIR__); // for .env
 	$dotenv->load();
-	$dotenv->required(["GOOGLE_API_KEY"]);
-	require("utilities/mapSign.php");
+	$dotenv->required(["GOOGLE_API_KEY"]);// required, since we'll be accessing the maps API
+	if ( getenv( GOOGLE_API_SECRET ) ){ // We should only have this function ready if we have an API Secret
+		require("utilities/mapSign.php");// A handy function for signing G-maps requests, adds security
+	}
 ?>
