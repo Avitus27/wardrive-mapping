dfc2f3ed0382056ff84f23dae89dc0a1448713ec
Mervyn Galvin <mervyn511@gmail.com>
2016-07-27 00:09:59 +0100

    Add filtering by various attributes

--- www/index.php
+++ www/index.php
@@ -1,11 +1,70 @@
 <?php
 	require_once("../components.php");
 
-	$statement = mysqli_stmt_init($mysqli);
+	//see what filters we have
+
+	$filters = array();
+	if(!empty($_POST["security"])){
+		$secFilter = array();
+		foreach ($_POST['security'] as $sec) {
+			$secFilter[] += $sec;
+		}
+		$filters[0] = $secFilter;
+	}
+
+	if(!empty($_POST["vulnerability"])) {
+		$vulFilter = array();
+		foreach ($_POST["vulnerability"] as $vul) {
+			$vulFilter[] += $vul;
+		}
+		$filters[1] = $vulFilter;
+	}
+	
+	$addOR = false;
 	$query = "SELECT * FROM `wigle`";
-	if(mysqli_stmt_prepare($statement, $query)){
+	if( isset($filters[0]) ){
+		$query .= " WHERE ";
+		if (in_array(1, $filters[0])) {
+			$query .= "AuthMode LIKE \"%WEP%\"";
+			$addOR = true;
+		}
+		if (in_array(2, $filters[0])) {
+			$query .= $addOR ? " OR " : "";
+			$query .= "AuthMode LIKE \"%WPA%\"";
+			$addOR = true;
+		}
+		if (in_array(3, $filters[0])) {
+			$query .= $addOR ? " OR " : "";
+			$query .= "AuthMode LIKE \"%WPS%\"";
+			$addOR = true;
+		}
+		if (in_array(4, $filters[0])) {
+			$query .= $addOR ? " OR " : "";
+			$query .= "AuthMode IS NULL OR AuthMode = \"\"";
+			$addOR = true;
+		}
+	}
+
+	if (isset($filters[1])) {
+		$UPCregex = "UPC[0-9]{7}";
+		$eircomregex = "eircom[0-9]{4} [0-9]{4}";
+		if (in_array(1, $filters[1])) {
+			$query .= $addOR ? " OR " : " WHERE ";
+			$query .= "SSID RLIKE '" . $UPCregex . "'";
+			$addOR = true;
+		}
+		if (in_array(2, $filters[1])) {
+			$query .= $addOR ? " OR " : " WHERE ";
+			$query .= "SSID RLIKE '" . $eircomregex . "'";
+			$addOR = true;
+		}
+	}
+
+	$statement = mysqli_stmt_init($mysqli);
+	if(mysqli_stmt_prepare($statement, $query)) {
 		mysqli_stmt_execute($statement);
 	}
+
 	mysqli_stmt_close($statement);
 	
 	$TABLE_1 = array();
@@ -17,21 +76,21 @@
 	}
 	mysqli_close($mysqli);
 
-	//var_dump($TABLE_1);
-	//die();
+
 ?>
 <html>
 	<head>
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 		<meta charset="utf-8">
-		<title>Simple markers</title>
+		<title>WiFi Mapper</title>
 		<style>
 			#map {
 				height: 60%;
 			}
 		</style>
 		<!-- Compiled and minified CSS -->
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+		<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	</head>
 	<body>
 		<div class="container">
@@ -43,33 +102,52 @@
 			</nav>
 			<!-- Options section -->
 			<div class="row">
-				<form class="col s12">
+				<form method="POST" class="col s12">
 					<div class="input-field col s12 m12 l4">
-						<select multiple>
+						<select name="security[]" multiple>
 							<option disabled="disabled">Select Any:</option>
-							<option value="1">WEP</option>
-							<option value="2">WPA</option>
-							<option value="3">WPS</option>
-							<option value="4">Open</option>
+							<option <?php
+							if (isset($filters[0]))
+								echo in_array(1, $filters[0]) == true ? "selected" : "";
+							?> value="1">WEP</option>
+							<option <?php
+							if (isset($filters[0]))
+								echo in_array(2, $filters[0]) == true ? "selected" : "";
+							?> value="2">WPA</option>
+							<option <?php
+							if (isset($filters[0]))
+								echo in_array(3, $filters[0]) == true ? "selected" : "";
+							?> value="3">WPS</option>
+							<option <?php
+							if (isset($filters[0]))
+								echo in_array(4, $filters[0]) == true ? "selected" : "";
+							?> value="4">Open</option>
 						</select>
 						<label>Security Type</label>
 					</div>
 					<div class="input-field col s12 m12 l4">
-						<select multiple>
+						<select name="vulnerability[]" multiple>
 							<option disabled="disabled">Select Any:</option>
-							<option value="1">UPC%07d</option>
-							<option value="2">eircom</option>
+							<?php
+							if( !isset($filters[1]) )
+								$filters[1] = array();
+							$i = 1;
+							foreach ($mappings as $mapping) {
+								if (in_array($i, $filters[1])) {
+									echo "<option selected value=\"{$i}\">{$mapping}</option>";
+								} else {
+									echo "<option value=\"{$i}\">{$mapping}</option>";
+								}
+								$i++;
+							}
+
+							?>
 						</select>
 						<label>Known Residence Vulnerability</label>
 					</div>
-					<div class="input-field col s12 m12 l4">
-						<select multiple>
-							<option value="1">WEP</option>
-							<option value="2">WPA</option>
-							<option value="3">Open</option>
-						</select>
-						<label>Security Type</label>
-					</div>
+					<button class="col s12 l3 right btn waves-effect waves-light" name="action" type="submit">Submit
+							<i class="material-icons right">send</i>
+					</button>
 				</form>
 			</div>
 			<!-- Map displayed here -->
@@ -109,13 +187,13 @@
 			  }
 			</script>
 			<script async defer
-				src="https://maps.googleapis.com/maps/api/js?key=<?php echo($_ENV['GOOGLE_API_KEY']) ?>&callback=initMap">
+				src="//maps.googleapis.com/maps/api/js?key=<?php echo($_ENV['GOOGLE_API_KEY']) ?>&callback=initMap">
 			</script>
 		</div>
 		<!-- jQuery and Materialize -->
-		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 		<!-- Compiled and minified JavaScript -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function() {
 				$('select').material_select();
